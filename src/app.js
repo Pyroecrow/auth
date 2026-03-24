5e97fbbdebc835ec7d6e44a9b3dedb124dc9b11e
Pyroecrow <alauritzen84@gmail.com>
2026-03-24 11:17:26 +0100

    jeg har lagt til data, routes og controllers for at kunne oprette en bruger, logge ind og logge ud. Jeg har også tilføjet en middleware til at beskytte ruter, der kræver autentificering.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./v1/routes/authRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/auth', authRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ src/v1/controllers/authcontroller.js
@@ -0,0 +1,22 @@
+
+
+//register and login user
+
+
+
+
+const register = async (req, res) => {
+    res.send("register user")
+}
+
+
+
+const login = async (req, res) => {
+    res.send("login user")
+}
+
+
+module.exports = {
+    register,
+    login
+}
