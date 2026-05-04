f3775eb8734daf2a3e1c99e49f5121f841320a6b
Pyroecrow <alauritzen84@gmail.com>
2026-05-04 09:24:13 +0200

    feat: add new semver range functions and improve user authentication
    - Implemented new semver range functions: gtr, ltr, intersects, max-satisfying, min-satisfying, min-version, outside, simplify, subset, to-comparators, and valid.
    - Updated package dependencies to include bcrypt for password hashing and jsonwebtoken for token generation.
    - Enhanced user registration functionality in authcontroller to hash passwords before storing them in the database.

--- src/v1/controllers/authcontroller.js
+++ src/v1/controllers/authcontroller.js
@@ -1,12 +1,38 @@
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //register and login user
 
+const pool = require('../data/db');
 
+const Secret_Key = "your_secret_key"; //NB! bruk .env for å lagre hemmelige nøkler i miljøvariabler
 
 
 const register = async (req, res) => {
-    res.send("register user")
+
+    const { Username, Password, Role } = req.body;
+
+    try {
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(Password, 10);
+
+        const [result] = await pool.query(
+            'INSERT INTO users (Username, Password, Role) VALUES (?, ?, ?)',
+            [Username, hashedPassword, Role]
+        );
+
+        res.status(201).json({
+            id: result.insertId,
+            username,
+            Password,
+            
+            role,
+            });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+
 }
 
 
